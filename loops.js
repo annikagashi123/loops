b94d5b969c39575c069b1b416ed5e7c99ff81879
annikagashi123 <annikagashi18@gmail.com>
2024-12-02 09:42:16 -0800

    afbae

---
+++ loops.js
@@ -0,0 +1,14 @@
+// for (let i = 9; i > 4; i++ ) {
+//     console.log(i);
+    
+// }
+// for (let i = 0; i < 10; i+=2) {
+//     console.log(i);
+    
+// }
+
+for (let i = 1; i < 100 ; i+=2) {
+    console.log(i);
+    
+}
+
