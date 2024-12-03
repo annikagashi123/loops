12a3dbbb68f6524d6f3c48e23b877d8b6afe187f
annikagashi123 <annikagashi18@gmail.com>
2024-12-03 09:39:48 -0800

    perfundova

--- loops.js
+++ loops.js
@@ -7,8 +7,25 @@
     
 // }
 
-for (let i = 1; i < 100 ; i+=2) {
-    console.log(i);
+// for (let i = 1; i < 100 ; i+=2) {
+//     console.log(i);
     
-}
+// }
+// for (let i = 1; i < 9; i++) {
+//     console.log(i);
+//   }
 
+// for (let i = 0; i < 9; i++) {   //mos me ta printu7 amanese e bojmebreak nalet te 6 edhe mas 6 nuk shkrun ma , nese dojme mi bo skip edhe8 te i==7 || i===8
+//     if (i===7) {
+//      continue;
+//     }
+//     console.log(i); 
+//  }
+let start , sstop;
+start = prompt("numri i 1");
+sstop = prompt("numri i2");
+console.log(start,sstop);
+
+for (let i = start; i < sstop; i++){
+    console.log(i);
+}
